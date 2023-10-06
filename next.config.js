5d57ef390f2d68e9575de51bdf92fce8e080a5dc
Andre Sena <andrediasdesena@gmail.com>
2023-10-06 16:32:59 -0300

    projeto Next.js - estrutura - config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
+const nextTranslate = require('next-translate-plugin');
+module.exports = nextTranslate({
+    reactStrictMode: true,
+  });
+  
 module.exports = nextConfig
