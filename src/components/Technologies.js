4bf150b23b843851d71dd963725c03988d02b850
Andre Sena <andrediasdesena@gmail.com>
2023-10-24 03:09:44 -0300

    fix: resolução img icons

--- src/components/Technologies.js
+++ src/components/Technologies.js
@@ -13,7 +13,6 @@ import {
   SiTailwindcss,
   SiTypescript,
   SiKotlin,
-  SiMysql,
   SiSpring,
   SiAmazonaws,
   SiVuedotjs
@@ -24,85 +23,85 @@ const technologiesItem = [
     id: "html",
     name: "HTML5",
     nivel: "avançado",
-    icon: <DiHtml5 className="text-[#E44D26] text-6xl" />
+    icon: <DiHtml5 className="text-[#E44D26] lg:text-6xl text-4xl" />
   },
   {
     id: "css",
     name: "CSS",
     nivel: "avançado",
-    icon: <DiCss3 className="text-[#1572B6] text-6xl" />
+    icon: <DiCss3 className="text-[#1572B6] lg:text-6xl text-4xl" />
   },
   {
     id: "js",
     name: "JavaScript",
     nivel: "avançado",
-    icon: <DiJsBadge className="text-[#F0DB4F] text-6xl" />
+    icon: <DiJsBadge className="text-[#F0DB4F] lg:text-6xl text-4xl" />
   },
   {
     id: "react",
     name: "React",
     nivel: "intermediário",
-    icon: <DiReact className="text-[#61DAFB] text-6xl" />
+    icon: <DiReact className="text-[#61DAFB] lg:text-6xl text-4xl" />
   },
   {
     id: "typescript",
     name: "TypeScript",
     nivel: "intermediário",
-    icon: <SiTypescript className="text-blue-600 text-6xl" />
+    icon: <SiTypescript className="text-blue-600 lg:text-6xl text-4xl" />
   },
   {
     id: "vue",
     name: "VUE",
     nivel: "básico",
-    icon: <SiVuedotjs className="text-[#42b983] text-6xl" />
+    icon: <SiVuedotjs className="text-[#42b983] lg:text-6xl text-4xl" />
   },
   {
     id: "sass",
     name: "Sass",
     nivel: "avançado",
-    icon: <SiSass className="text-pink-500 text-6xl" />
+    icon: <SiSass className="text-pink-500 lg:text-6xl text-4xl" />
   },
   {
     id: "tailwind",
     name: "Tailwind CSS",
     nivel: "avançado",
-    icon: <SiTailwindcss className="text-cyan-500 text-6xl" />
+    icon: <SiTailwindcss className="text-cyan-500 lg:text-6xl text-4xl" />
   },
   {
     id: "node",
     name: "Node.js",
     nivel: "intermediário",
-    icon: <DiNodejsSmall className="text-[#83CD29] text-6xl" />
+    icon: <DiNodejsSmall className="text-[#83CD29] lg:text-6xl text-4xl" />
   },
   {
     id: "java",
     name: "Java",
     nivel: "intermediário",
-    icon: <DiJava className="text-[#FF9800] text-6xl" />
+    icon: <DiJava className="text-[#FF9800] lg:text-6xl text-4xl" />
   },
   {
     id: "kotlin",
     name: "Kotlin",
     nivel: "intermediário",
-    icon: <SiKotlin className="text-purple-500 text-6xl" />
+    icon: <SiKotlin className="text-purple-500 lg:text-6xl text-4xl" />
   },
   {
     id: "springboot",
     name: "Spring Boot",
     nivel: "básico",
-    icon: <SiSpring className="text-green-600 text-6xl" />
+    icon: <SiSpring className="text-green-600 lg:text-6xl text-4xl" />
   },
   {
     id: "mysql",
     name: "MySQL",
     nivel: "intermediário",
-    icon: <DiMysql className="text-[#00618A] text-6xl" />
+    icon: <DiMysql className="text-[#00618A] lg:text-6xl text-4xl" />
   },
   {
     id: "aws",
     name: "AWS",
     nivel: "básico",
-    icon: <SiAmazonaws className="text-[#FF9900] text-6xl" />
+    icon: <SiAmazonaws className="text-[#FF9900] lg:text-6xl text-4xl" />
   }
 ];
 
@@ -120,13 +119,13 @@ export const Technologies = () => {
           <div class="flex flex-wrap justify-center gap-6">
             {chunk.map((tech) => (
               <div
-                class="flex items-center w-52 lg:w-72 gap-4 p-4 border-4 border-slate-600 rounded-lg"
+                class="flex lg:flex-row flex-col items-center w-40 lg:w-72 gap-4 lg:gap-8 p-4 border-4 border-slate-600 rounded-lg"
                 id={tech.id}
                 key={tech.id}
               >
                 {tech.icon}
-                <div class="text-left">
-                  <h3 class="lg:text-xl text-lg font-bold mb-1">{tech.name}</h3>
+                <div class="lg:text-left text-center">
+                  <h3 class="lg:text-xl text-base font-bold mb-1">{tech.name}</h3>
                   <span class="lg:text-sm text-xs">Nível:<p class="text-yellow-500">{tech.nivel}</p> </span>
                 </div>
               </div>
